2413b046f60be3407609eede577940460fba5512
ghh <guohaohao2012@vip.qq.com>
2023-05-19 12:46:36 +0800

    add prompt

--- src/default.ts
+++ src/default.ts
@@ -13,19 +13,19 @@ export const defaultMessage = `
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
 
 export const myMessage = `
-以下是我定义好的用户任务name和对应类的实现flowable:class，这些用户任务与flowable:class绑定才能使用，请记住在flowable业务流程你只能用我提供的用户任务，注意机器人要先导航到指定位置才能后续操作，所有活动统一用用户任务。
+以下是我定义好的用户任务name，请记住在flowable业务流程你只能用我提供的用户任务，注意机器人要先导航到指定位置才能后续操作，所有活动统一用用户任务。
 
-| name           | flowable:class                                            |
-| -------------- | --------------------------------------------------------- |
-| 导航去客厅     | org.flowable.ui.task.application.Servicetask.GoLivingRoom |
-| 导航去厨房     |                                                           |
-| 导航去卧室     |                                                           |
-| OCR处方识别    | org.flowable.ui.task.application.Servicetask.OpenOCR                                                          |
-| 打开客厅的灯   | org.flowable.ui.task.application.Servicetask.OpenLight    |
-| 关闭客厅的灯   | org.flowable.ui.task.application.Servicetask.CloseLight   |
-| 打开客厅的窗帘 | org.flowable.ui.task.application.Servicetask.OpenCurtain  |
-| 关闭客厅的窗帘 | org.flowable.ui.task.application.Servicetask.CloseLight   |
-| 控制卧室的风扇 | org.flowable.ui.task.application.Servicetask.WelTeacher   |
+| name          |
+| --------------|
+| 导航去客厅     |
+| 导航去厨房     |
+| 导航去卧室     | 
+| OCR处方识别    |
+| 打开客厅的灯   |
+| 关闭客厅的灯   |
+| 打开客厅的窗帘 | 
+| 关闭客厅的窗帘 |
+| 控制卧室的风扇 |
 
 下面我会告诉你我的具体需求，用我提供的用户任务构造业务流程，你可以做到吗。
 `
